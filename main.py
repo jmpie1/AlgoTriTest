c6931bc67bc2da088d50a8c65ae4a650eb3ec453
Jean Milou Pierre <miloujpierre@yahoo.fr>
2018-12-31 21:11:23 -0500

    Les trois algo de tri

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ myliste=TriListe([21,64,33,16,17,99,14,12,18,55])
 
 print(myliste.get_liste())
 #myliste.triSelection()
-#myliste.triInsertion()
-myliste.triAbulle()
+myliste.triInsertion()
+#myliste.triAbulle()
 print(myliste.get_liste())
 
